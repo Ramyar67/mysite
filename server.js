10d4ee2ad469b23923efea500d5b6af965fcfb56
Kamrannikookar <kamran.nikookar1375@gmail.com>
2026-08-15 04:24:51 +0430

    fix port for railway

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const const PORT = process.env.PORT || 3000;;
 
 // فایل‌های استاتیک (html, css, js) رو سرو کن
 app.use(express.static(__dirname));
